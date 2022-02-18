200015ac7ec0ffeac842adb1183ff8fb6e9fd3b3
André Rosa <25556799+andreffrosa@users.noreply.github.com>
2022-02-18 11:26:40 +0000

    fixed data upload

--- src/app.js
+++ src/app.js
@@ -157,35 +157,41 @@ async function fillSoilData() {
                 return merged_row;
             };
 
-            fs.createReadStream('./data/full_soil_data.csv')
-                .pipe(csv())
-                .on('data', (raw_row) => {
-                    row = convertRow(raw_row);
+            var file = './data/full_soil_data.csv'
+            if (fs.existsSync(file)) {
+                fs.createReadStream(file)
+                    .pipe(csv())
+                    .on('data', (raw_row) => {
+                        row = convertRow(raw_row);
 
-                    if( row.year == last_row?.year && row.location == last_row?.location ) {
-                        aux_buffer.push(row);
-                    } else {
+                        if( row.year == last_row?.year && row.location == last_row?.location ) {
+                            aux_buffer.push(row);
+                        } else {
+                            if(aux_buffer.length > 0) {
+                                // Merge entries
+                                to_insert.push(mergeRows(aux_buffer));
+                            }
+
+                            to_insert.push(row);
+                        }
+
+                        last_row = row;
+                    })
+                    .on('end', async () => {
                         if(aux_buffer.length > 0) {
                             // Merge entries
                             to_insert.push(mergeRows(aux_buffer));
                         }
 
-                        to_insert.push(row);
-                    }
+                        await Soil.create(to_insert);
 
-                    last_row = row;
-                })
-                .on('end', async () => {
-                    if(aux_buffer.length > 0) {
-                        // Merge entries
-                        to_insert.push(mergeRows(aux_buffer));
-                    }
-
-                    await Soil.create(to_insert);
-
-                    console.log(`Filled DB with ${to_insert.length} soil entries.`);
-                    resolve();
-                });
+                        console.log(`Filled DB with ${to_insert.length} soil entries.`);
+                        resolve();
+                    });
+            } else {
+                //console.log(`File ${file} does not exist.`);
+                resolve();
+            }
         } else {
             console.log("DB already filled with soil data.");
             resolve();
@@ -198,46 +204,53 @@ async function fillAirData() {
         const samples = await Air.find({}, null, {limit: 1});
         if(samples.length == 0) {
 
-            var files = ['historical_air_data.csv'];
+            var files = [];
+
+            if (fs.existsSync('historical_air_data.csv')) {
+                files.push('historical_air_data.csv');
+            }
+
             (await fs.promises.readdir('./data')).forEach( (item, i) => {
                 if(item.includes("_Daily")) {
                     files.push(item);
                 }
             });
 
+            if(files.lenght > 0) {
+                var promisses = [];
+                files.forEach((item, i) => {
+                    var path = './data/' + item;
 
-            var promisses = [];
-            files.forEach((item, i) => {
-                var path = './data/' + item;
+                    promisses.push(new Promise( async (resolve2, reject2) => {
+                        var n_lines = 0;
 
-                //console.log(path);
+                        fs.createReadStream(path)
+                            .pipe(csv({ separator: ';'}))
+                            .on('data', async (row) => {
+                                row['PM2_5'] = row['PM2.5'];
+                                delete row['PM2.5'];
+                                delete row[''];
 
-                promisses.push(new Promise( async (resolve2, reject2) => {
-                    var n_lines = 0;
+                                n_lines++;
 
-                    fs.createReadStream(path)
-                        .pipe(csv({ separator: ';'}))
-                        .on('data', async (row) => {
-                            row['PM2_5'] = row['PM2.5'];
-                            delete row['PM2.5'];
-                            delete row[''];
+                                await Air.create(row);
 
-                            n_lines++;
+                                delete row;
+                            })
+                            .on('end', () => {
+                                // console.log(path + " finished! " + n_lines);
+                                resolve2(n_lines);
+                            });
+                    }));
+                });
+                var n_lines = (await Promise.all(promisses)).reduce((a, b) => a+b);
 
-                            await Air.create(row);
-
-                            delete row;
-                        })
-                        .on('end', () => {
-                            // console.log(path + " finished! " + n_lines);
-                            resolve2(n_lines);
-                        });
-                }));
-            });
-            var n_lines = (await Promise.all(promisses)).reduce((a, b) => a+b);
-
-            console.log(`Filled DB with ${n_lines} air entries.`);
-            resolve();
+                console.log(`Filled DB with ${n_lines} air entries.`);
+                resolve();
+            } else {
+                //console.log(`File ${file} does not exist.`);
+                resolve();
+            }
         } else {
             console.log("DB already filled with air data.");
             resolve();
@@ -252,28 +265,33 @@ async function fillSoilManiaParameters() {
             // Insert Data
             var to_insert = [];
 
-            fs.createReadStream('./data/soilmania_soil_parameters.csv')
-                .pipe(csv({ separator: ';'}))
-                .on('data', (raw_row) => {
-                    var row = {
-                        time: new Date(raw_row.Date + " " + raw_row.Time).toISOString(),
-                        acidity: Number(raw_row["Acidity ()"]),
-                        oxygen_index: Number(raw_row["Oxygen Index ()"]),
-                        soil_conductivity: Number(raw_row["Soil conductivity (mS)"]),
-                        soil_moisture: Number(raw_row["Soil moisture (%)"]),
-                        soil_temperature: Number(raw_row["Soil temperature (°C)"])
-                    };
-                    to_insert.push(row);
-                })
-                .on('end', async () => {
-                    await SoilManiaParameters.create(to_insert);
+            var file = './data/soilmania_soil_parameters.csv'
+            if (fs.existsSync(file)) {
+                fs.createReadStream(file)
+                    .pipe(csv({ separator: ';'}))
+                    .on('data', (raw_row) => {
+                        var row = {
+                            time: new Date(raw_row.Date + " " + raw_row.Time).toISOString(),
+                            acidity: Number(raw_row["Acidity ()"]),
+                            oxygen_index: Number(raw_row["Oxygen Index ()"]),
+                            soil_conductivity: Number(raw_row["Soil conductivity (mS)"]),
+                            soil_moisture: Number(raw_row["Soil moisture (%)"]),
+                            soil_temperature: Number(raw_row["Soil temperature (°C)"])
+                        };
+                        to_insert.push(row);
+                    })
+                    .on('end', async () => {
+                        await SoilManiaParameters.create(to_insert);
 
-                    console.log(`Filled DB with ${to_insert.length} soilmania parameters entries.`);
+                        console.log(`Filled DB with ${to_insert.length} soilmania parameters entries.`);
 
-                    delete to_insert;
+                        delete to_insert;
 
-                    resolve();
-                });
+                        resolve();
+                    });
+            } else {
+                resolve();
+            }
         } else {
             console.log("DB already filled with soilmania parameters data.");
             resolve();
@@ -288,45 +306,50 @@ async function fillSoilManiaElements() {
             // Insert Data
             var to_insert = [];
 
-            fs.createReadStream('./data/soilmania_element_availability.csv')
-                .pipe(csv({ separator: ';'}))
-                .on('data', (raw_row) => {
-                    var row = {
-                        time: new Date(raw_row.Date + " " + raw_row.Time).toISOString(),
-                        Al: Number(raw_row["Al ()"]),
-                        As: Number(raw_row["As ()"]),
-                        B: Number(raw_row["B ()"]),
-                        Ca: Number(raw_row["Ca ()"]),
-                        Cd: Number(raw_row["Cd ()"]),
-                        Co: Number(raw_row["Co ()"]),
-                        Cu: Number(raw_row["Cu ()"]),
-                        Fe: Number(raw_row["Fe ()"]),
-                        Hg: Number(raw_row["Hg ()"]),
-                        K: Number(raw_row["K ()"]),
-                        Mg: Number(raw_row["Mg ()"]),
-                        Mn: Number(raw_row["Mn ()"]),
-                        Mo: Number(raw_row["Mo ()"]),
-                        N: Number(raw_row["N ()"]),
-                        Na: Number(raw_row["Na ()"]),
-                        Ni: Number(raw_row["Ni ()"]),
-                        P: Number(raw_row["P ()"]),
-                        Pb: Number(raw_row["Pb ()"]),
-                        S: Number(raw_row["S ()"]),
-                        Se: Number(raw_row["Se ()"]),
-                        Si: Number(raw_row["Si ()"]),
-                        Zn: Number(raw_row["Zn ()"]),
-                    };
-                    to_insert.push(row);
-                })
-                .on('end', async () => {
-                    await SoilManiaElements.create(to_insert);
+            var file = './data/soilmania_element_availability.csv'
+            if (fs.existsSync(file)) {
+                fs.createReadStream(file)
+                    .pipe(csv({ separator: ';'}))
+                    .on('data', (raw_row) => {
+                        var row = {
+                            time: new Date(raw_row.Date + " " + raw_row.Time).toISOString(),
+                            Al: Number(raw_row["Al ()"]),
+                            As: Number(raw_row["As ()"]),
+                            B: Number(raw_row["B ()"]),
+                            Ca: Number(raw_row["Ca ()"]),
+                            Cd: Number(raw_row["Cd ()"]),
+                            Co: Number(raw_row["Co ()"]),
+                            Cu: Number(raw_row["Cu ()"]),
+                            Fe: Number(raw_row["Fe ()"]),
+                            Hg: Number(raw_row["Hg ()"]),
+                            K: Number(raw_row["K ()"]),
+                            Mg: Number(raw_row["Mg ()"]),
+                            Mn: Number(raw_row["Mn ()"]),
+                            Mo: Number(raw_row["Mo ()"]),
+                            N: Number(raw_row["N ()"]),
+                            Na: Number(raw_row["Na ()"]),
+                            Ni: Number(raw_row["Ni ()"]),
+                            P: Number(raw_row["P ()"]),
+                            Pb: Number(raw_row["Pb ()"]),
+                            S: Number(raw_row["S ()"]),
+                            Se: Number(raw_row["Se ()"]),
+                            Si: Number(raw_row["Si ()"]),
+                            Zn: Number(raw_row["Zn ()"]),
+                        };
+                        to_insert.push(row);
+                    })
+                    .on('end', async () => {
+                        await SoilManiaElements.create(to_insert);
 
-                    console.log(`Filled DB with ${to_insert.length} soilmania element entries.`);
+                        console.log(`Filled DB with ${to_insert.length} soilmania element entries.`);
 
-                    delete to_insert;
+                        delete to_insert;
 
-                    resolve();
-                });
+                        resolve();
+                    });
+            } else {
+                resolve();
+            }
         } else {
             console.log("DB already filled with soilmania element data.");
             resolve();
